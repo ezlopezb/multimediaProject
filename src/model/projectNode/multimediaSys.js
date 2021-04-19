b271d3a165bc21fea2f26dc0d2f3d313c3a38dc2
ezlopezb <lopezezequiel9611@gmail.com>
2021-04-19 03:13:45 -0300

    cambios en el server de node
    pruebas con el resultado del json y la base de datos

--- src/model/projectNode/multimediaSys.js
+++ src/model/projectNode/multimediaSys.js
@@ -21,11 +21,11 @@ con.connect(function(err) {
   console.log("Connected!");
 });
 
-con.connect(function(err) {
-  con.query("SELECT * FROM Users", function (err, result, fields) {
-    console.log(result);
-  });
-});
+//con.connect(function(err) {
+  //con.query("SELECT * FROM Users", function (err, result, fields) {
+    //console.log(result);
+  //});
+//});
 
 
 //PROBAMOS EL GET A SPOTI
@@ -33,17 +33,61 @@ url = "";
 
 var querystring = require('querystring');
 var request = require('request');
-
+const dataset=new Array(); //tomamos una fila por dataset del track extraido y una columna por track
+var datarecords;
 request({
     headers: {
       'Accept': 'application/json' ,
       'Content-Type': 'application/json',
-      'Authorization': 'Bearer BQDVw5icpHwSEdtw3Fuah3sjz3HQFFV794-OAE0Ze5oHlM4weJcvXwCCDu3CrKZUEsbNdqnxL1FyvXnSgszpg6lr4LAFkqtuosZ1hTR3cO6T3yYKW7jxJYKBbNExqRtmTm3YpZZYyGN8zCYIYNKN6cmiia3A4NF3lCJDFOm-cisjm7L5uCnIStYOKG78wA2FZ-a28nyW_xVRcnLWhCjg96HkeLKmuz62XfJi55EEsHSV2Ml2-m_27SGKfLkycDW5LElfltI7jBcasEedoZbhC5qRYL7VvnkjgcbE5-yZgf36'
+      'Authorization': 'Bearer BQDkb0AWS_WA85iw0uZOwKAbwtpY7uwPMumWkKyi4RbVQqD_Z5nY0hK4hdRztXZM9GG7R7H2GRa2gNfynMXjmeBZWqvrGrUY_HslrLK_tsxJceWXaO3Mc31G2X9hghyAYqwRd17eZ9_wBhDR7QltZuyScUOb13AAD-HufIci9PXTa1CdA7WyfPIPzUimmih38USYMOOZbgWy_uXr6Om13frFkIKNwiC4CiGnW-X90a4tFFa2Zam31v59fJ4hLUUrubqzsor8Bwk-IO-ojyJmdNQJrUZr1UuoP8Mc6UXB7DdE'
     },
     uri: 'https://api.spotify.com/v1/albums/7rf1qZJ6hGSlPN7K9ShsVV',
     method: 'GET'
   }, function (err, res,data) {
-    var resultRaw=JSON.stringify(data);
     var resultado=JSON.parse(data);
-    console.log(resultado.tracks.items.name);
+    console.log("json cargado");
+    datarecords=resultado.tracks.items.length;
+    for(var i=0; i<datarecords; i++) {
+        dataset[i] =new Array(9);
+        for(var j=0; j<9; j++) {
+            dataset[i][j] = "";
+     
+          }
+    }
+    for(var i=0; i<resultado.tracks.items.length;i++){
+      dataset[i][0]="null";
+      dataset[i][1]=resultado.tracks.items[i].name.toString();
+      dataset[i][2]=(parseInt(resultado.tracks.items[i].duration_ms)/1000).toString();
+      dataset[i][3]="spotify";
+      dataset[i][4]=resultado.tracks.items[i].external_urls.spotify.toString();
+      dataset[i][5]=0;
+      dataset[i][6]=0;
+      dataset[i][7]=resultado.genres.toString();
+      dataset[i][8]=resultado.tracks.items[i].id.toString();
+    };
+    var query="insert into track";
+    for (var i=0; i<datarecords;i++){
+     query+=" values ("
+     for (var j=0; j<9;j++){
+        query+=dataset[i][j]+", ";
+        if (j=8){
+          query+=")";
+        }
+      }
+    }
+    console.log(query)
   });
+  var query="insert into track";
+  for (var i=0; i<datarecords;i++){
+    query+=" values (";
+    for (var j=0; j<9;j++){
+      query+=dataset[i][j]+", ";
+      if (j=8){
+        query+=")";
+      }
+    }
+  }
+  console.log(query)
+
+  
+
